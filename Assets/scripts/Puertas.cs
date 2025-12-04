6c04f5b5ce1d38a207e4caf5a70b2060a8b36fa0
solesitoazul06 <solesitoazul06@gmail.com>
2025-12-03 21:02:50 -0600

    Avance

--- Assets/scripts/Puertas.cs
+++ Assets/scripts/Puertas.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,7 +10,9 @@ public class Puertas : MonoBehaviour
     private bool jugadorEnRango = false;
     private bool SeMovio = false;
     public static bool SeMovera = false;
-    [SerializeField] GameObject Puerta;
+    public enum Direccion { Norte, Sur, Este, Oeste}
+    public Direccion direccion;
+    public float DistanciaSalto = 7f;
 
     private void Start()
     {
@@ -20,17 +23,38 @@ public class Puertas : MonoBehaviour
     {
         float distancia = Vector2.Distance(transform.position, player.position);
         jugadorEnRango = distancia < Radio;
+
         if (jugadorEnRango && Input.GetKeyDown(KeyCode.E) && !SeMovio && SeMovera)
         {
             Player.JugadorEnMovimiento = false;
             Debug.Log("En rango de una puerta");
             SeMovio = true;
             SeMovera = false;
+            Vector3 nuevaPos = player.position;
 
-            if(Puerta.transform.position.y < player.transform.position.y)
+            switch (direccion)
             {
-                Debug.Log("Puerta Norte Usada");
+                case Direccion.Norte:
+                    nuevaPos.y += DistanciaSalto;
+                    Debug.Log("Puerta Norte usada");
+                    break;
+
+                case Direccion.Sur:
+                    nuevaPos.y -= DistanciaSalto;
+                    Debug.Log("Puerta Sur usada");
+                    break;
+
+                case Direccion.Este:
+                    nuevaPos.x += DistanciaSalto;
+                    Debug.Log("Puerta Este usada");
+                    break;
+
+                case Direccion.Oeste:
+                    nuevaPos.x -= DistanciaSalto;
+                    Debug.Log("Puerta Oeste usada");
+                    break;
             }
+            player.position = nuevaPos;
         }
     }
 
