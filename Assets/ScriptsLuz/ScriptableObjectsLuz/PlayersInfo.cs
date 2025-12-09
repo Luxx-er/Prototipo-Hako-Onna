14e192743dee6c3fca0a9bdf8a77dfaef5ace92c
0D3TT3 <nataliaescalannte35@gmail.com>
2025-12-09 14:47:54 -0600

    Revert "Merge branch 'Luz' into Odette"
    This reverts commit df6e8855c75c389496481378becd74f4a737c365, reversing
    changes made to 6ca1c130c19341beae22e9c48020d95120f90403.

--- Assets/ScriptsLuz/ScriptableObjectsLuz/PlayersInfo.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[CreateAssetMenu(fileName = "PlayersInfo", menuName = "ScriptableObjects/PlayersInfo", order = 1)]
-
-public class PlayersInfo : ScriptableObject
-{
-    public int AmountPlayers;
-}
